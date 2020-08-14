6a64f7580d9bdec82ba735aad3f62d7a49f6fe45
Sibulele Kupelo <sibulelekupelo@gmail.com>
2020-08-14 05:02:10 +0200

    a. On click show textbox

--- components/phone-number-list/phone-number-list.js
+++ components/phone-number-list/phone-number-list.js
@@ -5,44 +5,18 @@ Component({
     phoneNumbers: [
       { name: "Kanya", number: "081 317 1046" },
       { name: "Neo", number: "081 317 2014" },
+      { name: "Kea", number: "081 317 2051" },
+      { name: "Kanya", number: "081 317 1046" },
+      { name: "Neo", number: "081 317 2014" },
+      { name: "Kea", number: "081 317 2051" },
+      { name: "Kanya", number: "081 317 1046" },
+      { name: "Neo", number: "081 317 2014" },
+      { name: "Kea", number: "081 317 2051" },
+      { name: "Kanya", number: "081 317 1046" },
+      { name: "Neo", number: "081 317 2014" },
       { name: "Kea", number: "081 317 2051" }
     ],
-    items: [
-      {
-        checked: true,
-        disabled: false,
-        value: "a",
-        title: "Checkbox - Checked by default",
-        id: "checkbox1"
-      },
-      {
-        checked: false,
-        disabled: false,
-        value: "b",
-        title: "Checkbox - Unchecked by default",
-        id: "checkbox2"
-      },
-      {
-        checked: true,
-        disabled: true,
-        value: "c",
-        title: "Checkbox - disabled checked by default",
-        id: "checkbox3"
-      },
-      {
-        checked: false,
-        disabled: true,
-        value: "d",
-        title: "Checkbox - disabled unchecked by default",
-        id: "checkbox4"
-      }
-    ],
-    items2: [
-      { name: "react", value: "React", checked: true },
-      { name: "vue", value: "Vue.js" },
-      { name: "ember", value: "Ember.js" },
-      { name: "backbone", value: "Backbone.js", disabled: true }
-    ]
+    isShowInput: false
   },
   onItemClick(ev) {
     my.alert({
@@ -58,6 +32,10 @@ Component({
       my.alert({
         title: `You are selecting the framework ${e.detail.value}`
       });
+    },
+
+    isShowInput() {
+      this.setData({ isShowInput: true });
     }
   }
 });
