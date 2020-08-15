d25b3f0c19a1c9970386f010e2ec89962726a5cd
Sibulele Kupelo <sibulelekupelo@gmail.com>
2020-08-15 15:13:26 +0200

    a. Add number into array

--- components/phone-number-list/phone-number-list.js
+++ components/phone-number-list/phone-number-list.js
@@ -6,15 +6,8 @@ Component({
       { name: "Kanya", number: "081 317 1046" },
       { name: "Neo", number: "081 317 2014" },
       { name: "Kea", number: "081 317 2051" },
-      { name: "Kanya", number: "081 317 1046" },
-      { name: "Neo", number: "081 317 2014" },
-      { name: "Kea", number: "081 317 2051" },
-      { name: "Kanya", number: "081 317 1046" },
-      { name: "Neo", number: "081 317 2014" },
-      { name: "Kea", number: "081 317 2051" },
-      { name: "Kanya", number: "081 317 1046" },
-      { name: "Neo", number: "081 317 2014" },
-      { name: "Kea", number: "081 317 2051" }
+      
+  
     ],
     isShowInput: false
   },
@@ -36,6 +29,18 @@ Component({
 
     isShowInput() {
       this.setData({ isShowInput: true });
+    },
+
+
+    onProceed() {
+
+      const phoneNumbers = this.data.phoneNumbers;
+
+      const newRecipient = { name: "Lerato", number: "078 427 9565"}
+      phoneNumbers.push(newRecipient);
+      console.log(phoneNumbers)
+      this.setData({ isShowInput: false, phoneNumbers });
+
     }
   }
 });
