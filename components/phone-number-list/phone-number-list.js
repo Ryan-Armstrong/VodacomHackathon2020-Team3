247c0e878d55af8a4305aea4465191760cda23f4
Sibulele Kupelo <sibulelekupelo@gmail.com>
2020-08-16 07:27:39 +0200

    b. Update list when press enter c. Set array data to filter

--- components/phone-number-list/phone-number-list.js
+++ components/phone-number-list/phone-number-list.js
@@ -1,7 +1,8 @@
 Component({
   mixins: [],
 
-  data: {
+  data: { 
+    value: 'food',
     phoneNumbers: [
       { name: "Kanya", number: "081 317 1046" },
       { name: "Neo", number: "081 317 2014" },
@@ -17,10 +18,15 @@ Component({
     });
   },
   props: {},
+
   didMount() {},
+
   didUpdate() {},
+  
   didUnmount() {},
+
   methods: {
+
     onChange(e) {
       my.alert({
         title: `You are selecting the framework ${e.detail.value}`
@@ -38,10 +44,57 @@ Component({
 
       const newRecipient = { name: "Lerato", number: "078 427 9565"}
       phoneNumbers.unshift(newRecipient);
-      
+
       console.log(phoneNumbers)
       this.setData({ isShowInput: false, phoneNumbers });
 
-    }
+    },
+
+
+checkAdult(value) {
+  return value >= 18;
+}
+
+,
+     handleInput(value) {
+
+
+        this.setData({
+          value,
+        });
+      },
+
+      handleClear(value) {
+        this.setData({
+          value: '',
+       
+        });
+      },
+
+      handleFocus() {},
+
+      handleBlur() {},
+
+      handleCancel() {
+        this.setData({
+          value: '',
+        });
+      },
+
+      handleSubmit(value) {
+      var phoneNumbers = this.data.phoneNumbers;
+      var lNames= phoneNumbers.filter(function(contact){ return contact.name ==value})
+       console.log(lNames);
+      
+       this.setData({
+          value: '',
+       phoneNumbers: lNames
+        });
+
+        // my.alert({
+        //   content: value,
+        // });
+      },
+
   }
 });
