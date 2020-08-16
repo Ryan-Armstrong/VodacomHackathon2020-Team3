942d4dccef02ead0530dce03cd5bceef0afd21ab
anthonyr02 <raglin.anthony@vodacom.co.za>
2020-08-16 21:31:21 +0200

    rebased to master, removed extra font files

--- .tea/entryFiles-development/index$.web.js
+++ .tea/entryFiles-development/index$.web.js
@@ -1,10 +1,6 @@
 require('@alipay/appx-compiler/lib/sjsEnvInit');
 require('./config$');
 
-require('../../components/bundle/bundle?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
-require('../../pages/buyBundle/buyBundle?hash=a9b48d7862c23db513325a890eec7d7fb315bfeb');
-require('../../pages/summary/summary?hash=a9b48d7862c23db513325a890eec7d7fb315bfeb');
-require('../../pages/order-status/order-status?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../components/customheading/customheading?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
 require('../../node_modules/mini-antui/es/list/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
 require('../../node_modules/mini-antui/es/list/list-item/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
@@ -12,4 +8,8 @@ require('../../node_modules/mini-antui/es/am-checkbox/index?hash=05d2a9730dd6009
 require('../../node_modules/mini-antui/es/search-bar/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
 require('../../components/phone-number-list/phone-number-list?hash=edca585a9d76ef4e6a18a489cff2d2600792174d');
 require('../../node_modules/mini-antui/es/footer/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
+require('../../components/bundle/bundle?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
 require('../../pages/recipient/recipient?hash=4509c58dd3ec5af9fd7dbab6020d3634374ccbf6');
+require('../../pages/buyBundle/buyBundle?hash=a9b48d7862c23db513325a890eec7d7fb315bfeb');
+require('../../pages/summary/summary?hash=a9b48d7862c23db513325a890eec7d7fb315bfeb');
+require('../../pages/order-status/order-status?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
